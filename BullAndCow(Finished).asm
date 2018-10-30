d938303cef674b3e045d6e5025c90a2b1b84cc38
Cuong Ngo <phucuongngo@gmail.com>
2018-10-30 15:38:50 -0500

    merge count bull and cow to main project code

--- BullAndCow(Finished).asm
+++ BullAndCow(Finished).asm
@@ -27,109 +27,117 @@ main:
 	syscall
 	sw $t1, guess
 	
+	jal count_bulls_and_cows
+	lw $a0, bull
+	li $v0, 1
+	syscall
+	
 	#End line
 	li $v0, 4
 	la $a0, endl
 	syscall
-
-outterWhile:
-	lw $t1, r_index		#t1 = r_index
-	bgt $t1, 3, endResult	#Finish condition (when r_index actually >3)
-
-	lw $t0, g_index
-	add $t0, $zero, $zero	#reset g_index to zero before loop through innerWhile
-	sw $t0, g_index
-
-innerWhile:
-	lw $t0, g_index		#t0 = g_index
-	lw $t1, r_index		#t1 = r_index
 	
-	bgt $t0, 3, inR		#increment r_index
-	
-	bne $t0, $t1, cowEqual		#if the 2 index dont match -> check for cow
-
-	la $t0, result
-	lw $t1, guess
-	
-	lw $s0, r_index
-	lw $s1, g_index
-	
-	#compare the 2 letter at index r (for result) and index g (for guess)
-	addu $t0, $t0, $s0
-	lbu $t2, ($t0)
-	
-	addu $t1, $t1, $s1
-	lbu $t3, ($t1)
-	
-	#if they are equal and by last condition of the 2 index being equal -> bull
-	beq $t2, $t3, bullEqual
-	j inG		#increment g_index
-	
-
-bullEqual:
-	lw $t0, bull
-	addi $t0, $t0, 1
-	sw $t0, bull
-	
-	j inG
-
-cowEqual:	#when the r_index != g_index -> go here
-	la $t0, result
-	lw $t1, guess
-	
-	lw $s0, r_index
-	lw $s1, g_index
-	
-	addu $t0, $t0, $s0
-	lbu $t2, ($t0)
-	
-	addu $t1, $t1, $s1
-	lbu $t3, ($t1)
-	
-	beq $t2, $t3, cowEqual2		#condition if the 2 index actual equal -> cow +1
-	j inG
-
-cowEqual2:
-	lw $t1, cow
-	addi $t1, $t1, 1
-	sw $t1, cow
-	
-inG:
-	lw $t0, g_index
-	addi $t0, $t0, 1
-	sw $t0, g_index
-	j innerWhile		#loop back again till g_index > 3
-
-inR:
-	lw $t0, r_index
-	addi $t0, $t0, 1
-	sw $t0, r_index
-	j outterWhile		#loop back again till r_index > 3
-	
-endResult:
-	#display the number of bull
+	lw $a0, cow
 	li $v0, 1
-	lw $t4, bull
-	move $a0, $t4
 	syscall
 	
-	li $v0, 4
-	la $a0, bullPrint
-	syscall
-	
-	li $v0, 1
-	lw $t4, cow
-	move $a0, $t4
-	syscall
-	
-	li $v0, 4
-	la $a0, cowPrint
-	syscall
-	
-	#end Line
-	li $v0, 4
-	la $a0, endl
-	syscall
 	
 	li $v0, 10
 	syscall
+
+	count_bulls_and_cows:
+		outterWhile:
+			lw $t1, r_index		#t1 = r_index
+			bgt $t1, 3, endResult	#Finish condition (when r_index actually >3)
+			#lw $t0, g_index
+			add $t0, $zero, $zero	#reset g_index to zero before loop through innerWhile
+			sw $t0, g_index
+
+		innerWhile:
+			lw $t0, g_index		#t0 = g_index
+			lw $t1, r_index		#t1 = r_index
+	
+			bgt $t0, 3, inR		#increment r_index
+	
+			bne $t0, $t1, cowEqual		#if the 2 index dont match -> check for cow
+
+			la $t0, result
+			lw $t1, guess
+	
+			lw $s0, r_index
+			lw $s1, g_index
+	
+			#compare the 2 letter at index r (for result) and index g (for guess)
+			addu $t0, $t0, $s0
+			lbu $t2, ($t0)
+	
+			addu $t1, $t1, $s1
+			lbu $t3, ($t1)
+	
+			#if they are equal and by last condition of the 2 index being equal -> bull
+			beq $t2, $t3, bullEqual
+			j inG		#increment g_index
+	
+		bullEqual:
+			lw $t0, bull
+			addi $t0, $t0, 1
+			sw $t0, bull
+			j inG
+
+		cowEqual:	#when the r_index != g_index -> go here
+			la $t0, result
+			lw $t1, guess
+			lw $s0, r_index
+			lw $s1, g_index
+	
+			addu $t0, $t0, $s0
+			lbu $t2, ($t0)
+	
+			addu $t1, $t1, $s1
+			lbu $t3, ($t1)
+	
+			beq $t2, $t3, cowEqual2		#condition if the 2 index actual equal -> cow +1
+			j inG
+
+		cowEqual2:
+			lw $t1, cow
+			addi $t1, $t1, 1
+			sw $t1, cow
+	
+		inG:
+			lw $t0, g_index
+			addi $t0, $t0, 1
+			sw $t0, g_index
+			j innerWhile		#loop back again till g_index > 3
+
+		inR:
+			lw $t0, r_index
+			addi $t0, $t0, 1
+			sw $t0, r_index
+			j outterWhile		#loop back again till r_index > 3
+	
+		endResult:
+			jr $ra
+			#display the number of bull
+		#	li $v0, 1
+		#	lw $t4, bull
+		#	move $a0, $t4
+		#	syscall
+		#
+		#	li $v0, 4
+		#	la $a0, bullPrint
+		#	syscall
+		#
+		#	li $v0, 1
+		#	lw $t4, cow
+		#	move $a0, $t4
+		#	syscall
+		#
+		#	li $v0, 4
+		#	la $a0, cowPrint
+		#	syscall
+		#
+		#	#end Line
+		#	li $v0, 4
+		#	la $a0, endl
+		#	syscall
--- Project2.asm
+++ Project2.asm
@@ -1,9 +1,20 @@
 .data
-	dict: .asciiz "zoicfoxyjawszipszingbruxcalxvextminxfauxoyezjogsquodcruxjagsjoysquinjudozigszagsgazebucklynxjoshjoeyhazepuckplexjigsquizjeuxjinxjockjackjumpjambjokyjivyjunkjimpjaukphizzoukzonkjukechezcozyzymemazyjoukqophjinkwhizfozyjokejakezebufujijowlpujajerkjaupjivejaggzeksjupefuzeputzhazykojizincfutzjubazerkjucojubequipwaxyjehujugsjowsdozylazyfluxmazeczarfazepixyjohnboxyjibejugajibsbizejuryjobsprezjabsfrizjapepoxyzepsjamsquayzanyyutzzapsqueyzarfquaghadj"
-	correct_word: .space 4									# used to store correct word 
-	random_number: .word 4									# used to store random number
-	random_max_value: .word 110								# used to set max number of word
-	inp:	.space 5
+	dict: 				.asciiz "zoicfoxyjawszipszingbruxcalxvextminxfauxoyezjogsquodcruxjagsjoysquinjudozigszagsgazebucklynxjoshjoeyhazepuckplexjigsquizjeuxjinxjockjackjumpjambjokyjivyjunkjimpjaukphizzoukzonkjukechezcozyzymemazyjoukqophjinkwhizfozyjokejakezebufujijowlpujajerkjaupjivejaggzeksjupefuzeputzhazykojizincfutzjubazerkjucojubequipwaxyjehujugsjowsdozylazyfluxmazeczarfazepixyjohnboxyjibejugajibsbizejuryjobsprezjabsfrizjapepoxyzepsjamsquayzanyyutzzapsqueyzarfquaghadj"
+	correct_word: 		.space 4									# used to store correct word 
+	random_number: 		.word 4										# used to store random number
+	random_max_value: 	.word 110									# used to set max number of word
+	bullPrint:			.asciiz "\nBull= "
+	cowPrint:			.asciiz "\nCow= "
+	#result:				.asciiz "NOAR"								# assigned word
+	input:				.asciiz "\nGet input: "
+	buffer:				.space	36
+	endl:				.asciiz "\n"
+	guess_word_index:			.word 0										# get the next index
+	correct_word_index:			.word 0										# count the correct_word_index till it reaches 4 then ends
+	bull: 				.word 0
+	cow:				.word 0
+	guess:				.asciiz										# holds the input of the user
+	inp:				.space 5
 .text
 
 	main:
@@ -16,33 +27,79 @@
  		li $v0, 4
  		syscall
  	  ################## end of generate random word ############################	
- 				
-		#Print prompt
-		li	$v0, 4
-		#la	$a0, Prmpt
-		syscall
-		#Get input string
-		li	$v0, 8
-		la	$a0, inp
-		li	$a1, 5
-		syscall
-		lb	$t0, inp
-		li	$t1, 33 # !
-		#beq	$t0, $t1, giveup
-		slti	$t1, $t0, 97
-		li	$t2, 122
-		slt	$t2, $t2, $t0
-		and	$t1, $t1, $t2
-		#beq
+ 	  
+ 	  
+ 	  # get input from user
+ 			#print out "Get input: "
+			li $v0, 4
+			la $a0, input
+			syscall
 	
-	li $v0, 10											# end of main, below of this code are all functions
-	syscall
+			#get string
+			li $v0, 8
+			la $a0, buffer
+			li $a1, 5
+			move $t1,$a0 	#save string to t0
+			syscall
+			sw $t1, guess	  
+ 	  	
+ 	  # validation code goes here					
+ 	  	#### Sean's code #####		
+			#Print prompt
+			#li	$v0, 4
+			#la	$a0, Prmpt
+			#syscall
+			#Get input string
+			#li	$v0, 8
+			#la	$a0, inp
+			#li	$a1, 5
+			#syscall
+			#lb	$t0, inp
+			#li	$t1, 33 # !
+			#beq	$t0, $t1, giveup
+			#slti	$t1, $t0, 97
+			#li	$t2, 122
+			#slt	$t2, $t2, $t0
+			#and	$t1, $t1, $t2
+			#beq
+			
+			
+		# call bull and count if validation is success
+		
+		# play sound
+		jal success_sound
+		
+		# print bull and cow
+		jal count_bulls_and_cows
+		
+		
+		
+		# print bull
+		li $v0, 4
+		la $a0, bullPrint
+		syscall
+		lw $a0, bull
+		li $v0, 1
+		syscall
+	
+		# print cow
+		li $v0, 4
+		la $a0, cowPrint
+		syscall
+		lw $a0, cow
+		li $v0, 1
+		syscall
+
+		
+		li $v0, 10											# end of main, below of this code are all functions
+		syscall
 
 ###################################################################################################################################
 ##																																 ##
-##													FUNCTION SECTION															 ##
+##													FUNCTIONS SECTION															 ##
 ##																																 ##
 ###################################################################################################################################
+	
 	success_sound:										# MIDI sound
 		li $v0, 31
 		li $a0, 72		 								# C pitch (0-127)
@@ -81,3 +138,79 @@
  			j start_loop_123
  		exit_loop_123:
  		jr $ra
+ 	
+ 	######### count bulls and cows function ###########	
+ 	count_bulls_and_cows:
+		outterWhile:
+			lw $t1, correct_word_index					#t1 = correct_word_index
+			bgt $t1, 3, endResult						#Finish condition (when correct_word_index actually >3)
+			#lw $t0, guess_word_index
+			add $t0, $zero, $zero						#reset guess_word_index to zero before loop through innerWhile
+			sw $t0, guess_word_index
+
+		innerWhile:
+			lw $t0, guess_word_index					#t0 = guess_word_index
+			lw $t1, correct_word_index					#t1 = correct_word_index
+	
+			bgt $t0, 3, inR								#increment correct_word_index
+	
+			bne $t0, $t1, cowEqual						#if the 2 index dont match -> check for cow
+
+			la $t0, correct_word
+			lw $t1, guess
+	
+			lw $s0, correct_word_index
+			lw $s1, guess_word_index
+	
+			#compare the 2 letter at index of correct word and index of guessed word 
+			addu $t0, $t0, $s0
+			lbu $t2, ($t0)
+	
+			addu $t1, $t1, $s1
+			lbu $t3, ($t1)
+	
+			#if they are equal and by last condition of the 2 index being equal -> bull
+			beq $t2, $t3, bullEqual
+			j inG										#increment guess_word_index
+	
+		bullEqual:
+			lw $t0, bull
+			addi $t0, $t0, 1
+			sw $t0, bull
+			j inG
+
+		cowEqual:										#when the correct_word_index != guess_word_index -> go here
+			la $t0, correct_word
+			lw $t1, guess
+			lw $s0, correct_word_index
+			lw $s1, guess_word_index
+	
+			addu $t0, $t0, $s0
+			lbu $t2, ($t0)
+	
+			addu $t1, $t1, $s1
+			lbu $t3, ($t1)
+	
+			beq $t2, $t3, cowEqual2						#condition if the 2 index actual equal -> cow +1
+			j inG
+
+		cowEqual2:
+			lw $t1, cow
+			addi $t1, $t1, 1
+			sw $t1, cow
+	
+		inG:
+			lw $t0, guess_word_index
+			addi $t0, $t0, 1
+			sw $t0, guess_word_index
+			j innerWhile								#loop back again till guess_word_index > 3
+
+		inR:
+			lw $t0, correct_word_index
+			addi $t0, $t0, 1
+			sw $t0, correct_word_index
+			j outterWhile								#loop back again till correct_word_index > 3
+	
+		endResult:
+			jr $ra										# jump back to main
+	########## end count bull and cow function ##################
